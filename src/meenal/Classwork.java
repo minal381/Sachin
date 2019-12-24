4c7f9f22262c854068e27a5eddfcb4964796d80d
Meenal Musale <meenalmusale@Sachins-Air.home>
2019-12-23 22:31:26 -0500

    updated classwork

---
+++ src/meenal/Classwork.java
@@ -0,0 +1,27 @@
+package meenal;
+
+public class Classwork {public static void main(String[] args) {
+    int age = 35;
+    boolean isChild = age<= 7;
+    System.out.println(isChild);
+    boolean isSenior = age>= 65;
+    System.out.println(isSenior);
+
+    int x = 20 ;
+    int y = 2  ;
+    int add = x + y;
+    System.out.println(add);
+
+    int substract = x - y;
+    System.out.println(substract);
+
+    int multiplication = x * y;
+    System.out.println(multiplication);
+
+    int division = x/y;
+    System.out.println(division);
+    
+}
+
+
+    }
