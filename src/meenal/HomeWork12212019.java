b021c064db7af3029b4fb375337db8352b62a835
Meenal Musale <meenalmusale@Sachins-Air.home>
2019-12-27 14:47:55 -0500

    HomeWork1221

---
+++ src/meenal/HomeWork12212019.java
@@ -0,0 +1,38 @@
+package meenal;
+
+public class HomeWork12212019 {
+    public static void main(String[] args) {
+        // 1. Celsius to Kelvin
+           // T(K) = T(°C) + 273.15
+
+        double celsius = 0;
+        double kelvin = celsius + 273.15;
+        System.out.println("Temperature in Kelvin" + " "+"="+ " "+ kelvin + (char)0x00B0 +" " + "K");
+
+        // 2. Celsius to Fahrenheit
+           // T(°F) = T(°C) × 9/5 + 32
+
+        double celsius1 = 0;
+        double fahrenheit = celsius1 * 9/5 +32;
+        System.out.println("Temperature in Fahrenheit" +" " + "="+" "+fahrenheit +(char)0x00B0+ " " + "F");
+
+        // 3. Kelvin to Celsius
+        // T(°C) = T(K) - 273.15
+
+        double kelvin1 = 0;
+        double celsius2 = kelvin1 - 273.15;
+        System.out.println("Temperature in Celsius" + " " + "=" + " " + celsius2 + (char)0x00B0 + "C" );
+
+        // 4. Kelvin to Fahrenheit
+        // T(°F) = T(K) × 9/5 - 459.67
+
+        double kelvin2 = 0;
+        double fahrenheit1 = kelvin2 * 9/5 - 459.67;
+        System.out.println("Temperature in Fahrenheit" + " " + "=" + " " + fahrenheit1+(char)0x00B0 + "F");
+
+
+
+
+
+    }
+}
